5d49bd057ea7b993f1d62562badfce62dfd1aaa0
Andrew Castle <andrewcastle510@gmail.com>
2022-08-02 15:28:55 -0400

    initial stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Bookshelf from './Components/Bookshelf';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
--- src/Components/Bookshelf.js
+++ src/Components/Bookshelf.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import TextField from '@mui/material/TextField';
 
 export default function Bookshelf(){
 
+    const [fetchLink, setFetchLink] = useState('/test')
+    const [data, setData] = useState('')
+    const [search, setSearch] = useState('');
+
+
+    useEffect(() => {
+        console.log(fetchLink);
+        fetch(fetchLink).then(response=>
+          response.json().then(res => {
+            console.log(res);
+            setData(res);
+          })
+    
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [fetchLink]);
+
+    // Handling the name change
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
     return (
         <div>   
-
+            <TextField required fullWidth id="search" label="Search"
+            variant="outlined" size="small" onChange={handleSearch}/>
             <h1> This is a component! </h1>
+
+            <h2> {data} </h2>
 
         </div>
 
